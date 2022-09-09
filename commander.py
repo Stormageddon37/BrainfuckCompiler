c26b8eb5f9ab826700467d819ac1ab8833ca9ca3
Nitzan <bresler.nitzan@gmail.com>
2022-09-09 15:14:03 +0300

    Minor changes to commander

--- commander.py
+++ commander.py
@@ -5,11 +5,8 @@ def initialize_program():
 	return f"""
 	public class Main {{
 		public static void main(String[] args) {{
-		{initialize_array()}
+		int pointer = 0;
+		byte[] arr = new byte[{config.MEMORY_SIZE}];
 		}}
 	}}
 	"""
-
-
-def initialize_array():
-	return f'byte[] arr = new byte[{config.MEMORY_SIZE}];'
