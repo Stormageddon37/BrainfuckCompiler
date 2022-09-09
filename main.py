91e2b9f39a75361221f1853f0e8840a78d245d18
Nitzan <bresler.nitzan@gmail.com>
2022-09-09 15:58:10 +0300

    Main runs and compiles

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from commander import initialize_program
 from compiler import compile_and_execute_file
-from utils import write_to_file
+from utils import write_to_file, read_file
 
 
 def main():
-	write_to_file('Main.java', initialize_program())
+	write_to_file('Main.java', initialize_program(read_file('input.bf')))
 	compile_and_execute_file('Main')
 
 
