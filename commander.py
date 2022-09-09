8c9c8d15e8c8d062cb7d56b8f645c258216e43b9
Nitzan <bresler.nitzan@gmail.com>
2022-09-09 15:58:39 +0300

    Commander now works with memory and instruction seperately. oops

--- commander.py
+++ commander.py
@@ -1,12 +1,27 @@
 import config
 
 
-def initialize_program():
+def initialize_program(bf_code):
 	return f"""
 	public class Main {{
+		private static byte[] memory = new byte[{config.MEMORY_SIZE}];
+		private static char[] code = "{bf_code}".toCharArray();
+		
+		private static int instruction_pointer = 0;
+		private static int memory_pointer = 0;		
+		
+		public static int nextClosingBracket(int index) {{ 
+			for(int i = index; i < code.length; i++) {{
+				if (code[i] == ']') {{
+					return i+1;
+				}}
+			}}
+			return index;
+		}}
+		
 		public static void main(String[] args) {{
-		int pointer = 0;
-		byte[] arr = new byte[{config.MEMORY_SIZE}];
+			System.out.println("hello");
+			System.out.println(code);
 		}}
 	}}
 	"""
